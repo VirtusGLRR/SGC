e6d7d9cd82bca5ffee3f6e766ef38fffaebe9af3
Ryan <ryansoaresjorvino@gmail.com>
2025-12-13 20:59:12 -0300

    fix: Correção de import relativo na pasta conftest.py

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+import sys
+
+backend_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, backend_dir)
 
 from database.database import Base
 
