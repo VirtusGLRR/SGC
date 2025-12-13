28db9141e13a8ff481b979a08fc70f3640ba5ac9
Ricken Diniz <dinizrickenn@gmail.com>
2025-12-13 18:38:35 -0300

    feat: add Receita_Item model

---
+++ backend/models/receita_item.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, ForeignKey, Numeric
+from sqlalchemy.orm import relationship
+from app.database.base import Base
+
+class ReceitaItem(Base):
+    __tablename__ = "Receita_Item"
+
+    receita_id = Column(
+        Integer,
+        ForeignKey("Receita.id"),
+        primary_key=True
+    )
+    item_id = Column(
+        Integer,
+        ForeignKey("Item.id"),
+        primary_key=True
+    )
+
+    quantidade = Column(Numeric, nullable=False)
+
+    receita = relationship("Receita", back_populates="receita_itens")
+    item = relationship("Item", back_populates="receita_itens")
