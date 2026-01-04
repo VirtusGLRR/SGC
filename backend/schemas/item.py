6db2f9c24624645e9caea76ee658bc7412860e03
Ryan <ryansoaresjorvino@gmail.com>
2026-01-03 22:02:31 -0300

    fix: update item schema to comport the column price_unit

--- backend/schemas/item.py
+++ backend/schemas/item.py
@@ -8,6 +8,7 @@ class ItemBase(BaseModel):
     amount: float
     description: Optional[str] = None
     price: Optional[float] = 0
+    price_unit = str = "unidade"
     expiration_date: Optional[datetime] = None
     create_at: Optional[datetime] = Field(default_factory=datetime.now) 
     update_at: Optional[datetime] = None
